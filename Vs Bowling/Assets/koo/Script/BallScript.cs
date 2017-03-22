98e0fb135bad3955ae6a4fa38a0e5bff69f7edf1
Fukuda Kouki <e165727@ie.u-ryukyu.ac.jp>
2017-03-22 14:43:03 +0900

    add Script

---
+++ Vs Bowling/Assets/koo/Script/BallScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+	private float speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody>().AddForce(
+			(transform.forward) * speed,
+		ForceMode.VelocityChange);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Vs Bowling/Assets/koo/Script/PinScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinScript : MonoBehaviour {
+
+	float PinX;
+	bool FallPin;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		PinX = this.gameObject.transform.rotation.x;
+		if(60.0f < PinX){
+			FallPin = true;
+		}
+		print(FallPin);
+	}
+}
