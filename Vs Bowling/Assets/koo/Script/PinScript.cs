806fef3a6dc3603485c5a7087e2b3e6f41e132a4
Fukuda Kouki <e165727@ie.u-ryukyu.ac.jp>
2017-03-22 16:28:38 +0900

    add all

--- Vs Bowling/Assets/koo/Script/PinScript.cs
+++ Vs Bowling/Assets/koo/Script/PinScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PinScript : MonoBehaviour {
 
 	float PinX;
+	float PinZ;
 	bool FallPin;
 	// Use this for initialization
 	void Start () {
@@ -13,10 +14,14 @@ public class PinScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		PinX = this.gameObject.transform.rotation.x;
-		if(60.0f < PinX){
+		PinX = this.gameObject.transform.rotation.eulerAngles.x;
+		PinZ = this.gameObject.transform.rotation.eulerAngles.z;
+		if(60.0f < PinX && PinX < 90.0f || -60.0f > PinX && PinX > -90.0f ||
+		   60.0f < PinZ && PinZ < 90.0f || -60.0f > PinZ && PinZ > -90.0f){
 			FallPin = true;
 		}
 		print(FallPin);
+		print (PinX);
+		print (PinZ);
 	}
 }
