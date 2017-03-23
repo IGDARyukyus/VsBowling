b04421e1c943a93e818cddb46f7a98e2ccfc6dc1
Fukuda Kouki <e165727@ie.u-ryukyu.ac.jp>
2017-03-23 11:12:00 +0900

    add all

--- Vs Bowling/Assets/koo/Script/BallScript.cs
+++ Vs Bowling/Assets/koo/Script/BallScript.cs
@@ -6,16 +6,26 @@ using UnityEngine.UI;
 public class BallScript : MonoBehaviour {
 	private float speed = 25.0f;
 	Slider m_slider;
+	//Slider m2_slider;
 	GameObject PowerUI;
+	//GameObject AngleUI;
 	int pushCount;
 	// Use this for initialization
 	void Start () {
 		PowerUI = GameObject.Find("PowerUI");
 		m_slider = PowerUI.GetComponent<Slider> ();
+		//AngleUI = GameObject.Find ("AngleUI");
+		//m2_slider = AngleUI.GetComponent<Slider>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown (KeyCode.A) && this.transform.position.x > -3) {
+			this.transform.position += new Vector3 (-1, 0, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.D) && this.transform.position.x < 3){
+			this.transform.position += new Vector3 (1, 0, 0);
+		}
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			pushCount += 1;
 		}
@@ -29,7 +39,15 @@ public class BallScript : MonoBehaviour {
 			this.GetComponent<Rigidbody> ().AddForce (
 				(transform.forward) * speed * m_slider.value,
 				ForceMode.VelocityChange);
-			pushCount = 0;
 		}
+		/*if (pushCount == 1) {
+			m2_slider.value += 1 * Time.deltaTime;
+		}
+		if (m2_slider.value == 1) {
+			m2_slider.value = 0;
+		}
+		if (pushCount == 2) {
+			transform.Rotate(new Vector3(0, 90, 0)*(m2_slider.value -0.5);
+		}*/
 	}
 }
